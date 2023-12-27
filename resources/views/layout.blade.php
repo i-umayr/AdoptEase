3ba8441fcd154c1df8ba3cd157a9a5061bb90bdf
Emaan Umer <emaanumer012@gmail.com>
2023-12-27 20:01:37 +0500

    added-stuff

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,9 +3,33 @@
 <head>
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <link
+            href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,,500,600,700"
+            rel="stylesheet"
+        />
+    <link href="css/animate.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/animate.css"/>
+        <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css" />
+        <link rel="stylesheet" href="css/animate.css" />
+
+        <link rel="stylesheet" href="css/owl.carousel.min.css" />
+        <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+        <link rel="stylesheet" href="css/magnific-popup.css" />
+
+        <link rel="stylesheet" href="css/aos.css" />
+
+        <link rel="stylesheet" href="css/ionicons.min.css" />
+
+        <link rel="stylesheet" href="css/bootstrap-datepicker.css" />
+        <link rel="stylesheet" href="css/jquery.timepicker.css" />
+
+        <link rel="stylesheet" href="css/flaticon.css" />
+        <link rel="stylesheet" href="css/icomoon.css" />
+        <link rel="stylesheet" href="css/style.css" />
     <style>
        html {
   height: 100%;
@@ -13,11 +37,9 @@
 body {
   margin:0;
   padding:0;
-  font-family: sans-serif;
-  background: linear-gradient(#141e30, #243b55);
-}
+  font-family: sans-serif;}
   
-    </style>
+</style>
 </head>
 <body >
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
